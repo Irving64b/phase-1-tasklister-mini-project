d2b51f9f288ffa9b1635abe032854da5e23b12f8
Irving Medina <irving.64@hotmail.com>
2023-05-26 00:01:35 -0400

    complete mini

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskList = new TaskList();
+
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newDueDate = document.getElementById("new-due-date");
+
+  const taskUl = document.getElementById("tasks");
+
+  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
+
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskList.createNewTask(newTaskDescription.value + " - " + newDueDate.value);
+    
+    e.target.reset();
+    renderApp();
+  });
+
+  taskUl.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      taskList.deleteTask(e.target.dataset.description);
+      renderApp();
+    }
+  });
 });
+class Task {
+  constructor(description) {
+    this.description = description;
+  }
+
+  render() {
+    return `
+      <li>
+        ${this.description}
+        <button data-description="${this.description}">X</button>
+      </li>
+      `;
+  }
+}
+class TaskList {
+  constructor() {
+    this.tasks = [];
+  }
+
+  createNewTask(description) {
+    const newTask = new Task(description);
+    this.tasks.push(newTask);
+  }
+
+  renderTasks() {
+    return this.tasks.map((task) => task.render()).join("");
+  }
+
+  deleteTask(description) {
+    this.tasks = this.tasks.filter((task) => task.description !== description);
+  }
+}
